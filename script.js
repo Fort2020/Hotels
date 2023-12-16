0195f340c87ea7b7ff5a2d952815046ead8b064e
fort2020 <frankfortdata@gmail.com>
2023-12-16 22:48:22 +0000

    Trying To Build Home Contents

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const body = document.querySelector("body"),
       toggle = body.querySelectorAll(".toggle"),
       searchBtn = body.querySelector(".search-box"),
       modeSwitch = body.querySelector(".toggle-switch"),
-      modeText = body.querySelector(".mode-text");
+      modeText = body.querySelector(".mode-text"),
+      homeSection = document.querySelector(".home");
+
+// Function to load content into the right window
+const loadContent = (content) => {
+  homeSection.innerHTML = content;
+};
 
 modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
@@ -14,13 +20,37 @@ searchBtn.addEventListener("click", () => {
 });
 
 toggle.forEach((button) => {
-  button.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-  
-    if(body.classList.contains("dark")) {
-      modeText.innerText = "Light Mode"
-    } else {
-      modeText.innerText = "Dark Mode"
+  button.addEventListener("click", (event) => {
+    const target = event.target;
+
+    // Check if the clicked button is the "Home" link
+    if (target.tagName === "A" && target.innerText === "HOME") {
+      console.log("Home link clicked");
+
+      // Load the content of home.html into the right window
+      fetch("home.html")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Failed to fetch: ${response.status}`);
+          }
+          return response.text();
+        })
+        .then((htmlContent) => {
+          console.log("Content fetched successfully");
+          loadContent(htmlContent);
+        })
+        .catch((error) => {
+          console.error("Error fetching content:", error);
+        });
     }
-    });
+
+    sidebar.classList.toggle("close");
+    homeSection.classList.toggle("move-right");
+
+    if (body.classList.contains("dark")) {
+      modeText.innerText = "Light Mode";
+    } else {
+      modeText.innerText = "Dark Mode";
+    }
+  });
 });
